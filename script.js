fb3ac2c68566d611493149acd3c869defba31dba
saintelian <peraltaelian02@gmail.com>
2026-09-15 17:49:38 -0500

    Update burger descriptions and images to use Pretzel bread

--- script.js
+++ script.js
@@ -66,15 +66,15 @@ const data = {
   ],
 
   BURGERS: [
-    { name: "La Jefa", desc: "Pan Pretzel con filete de pescado apanado, cebolla encurtida, lechuga romana,  mix de quezos mozzarella y cheddar. Servida con papas. ", price: "$42.000", img: "IMAGENES/lajefa.jpg" },
-    { name: "Chori Burger", desc: "Pan premiun con ajonjoli negro acompañada con carne de 130gr, chorizo, mozzarella, salsa chipotle, tomate y lechuga. Servida con papas.", price: "$40.000", img: "IMAGENES/chori.jpg" },
-    { name: "Burger Crispy Cesar", desc: "Pan premiun con ajonjoli negro acompañada pollo apanado, mozzarella y salsa de ajo, con mix de lechugas y parmesano. Servida con papas.", price: "$36.000", img: "IMAGENES/crisp.jpg" },
-    { name: "Cheese Bacon Burger", desc: "Pan premiun con ajonjoli negro acompañada con carne burger, queso amarillo, tocineta, cebolla roja y pepinillos. Servida con papas.", price: "$33.000", img: "IMAGENES/bacon.jpg" },
-    { name: "Burger OG", desc: "Pan premiun con ajonjoli negro acompañada con carne de 130gr, doble queso, chorizo artesanal, tocineta y salsas de la casa. Servida con papas.", price: "$48.000", img: "IMAGENES/burgerog.jpg" },
-    { name: "Spicy & Sweet Burger", desc: "Pan premiun con ajonjoli negro acompañada con carne burger carne de 130gr, mozzarella, pimentones caramelizados y salsa de la casa. Servida con papas.", price: "$33.000", img: "IMAGENES/spicy.jpg" },
-    { name: "Burger Whopper", desc: "Pan premiun con ajonjoli negro acompañada con carne burger carne de 130gr, mozzarella apanado, cebolla caramelizada y tocineta. Servida con papas.", price: "$46.000", img: "IMAGENES/whop.jpg" },
-    { name: "The Big Boss", desc: "Pan premiun con ajonjoli negro acompañada con carne doble de 130gr, doble queso cheddar, doble tocineta y salsa de la casa. Servida con papas.", price: "$46.000", img: "IMAGENES/hamburguesan.png" },
-    { name: "La Patrona", desc: "Pan premiun con ajonjoli negro acompañada con carne doble de 130gr, piña caramelizada, salsa de la casa, Dip dulce & salado, lechuga y tomate. Servida con papas.", price: "$48.000", img: "IMAGENES/lapatro.jpg" },
+    { name: "La Jefa", desc: "Pan Pretzel acompañado con filete de pescado apanado, cebolla encurtida, lechuga romana,  mix de quezos mozzarella y cheddar. Servida con papas. ", price: "$42.000", img: "IMAGENES/lajefa.jpg" },
+    { name: "Chori Burger", desc: "Pan Pretzel acompañado con carne de 130gr, chorizo, mozzarella, salsa chipotle, tomate y lechuga. Servida con papas.", price: "$40.000", img: "IMAGENES/chori.jpg" },
+    { name: "Burger Crispy Cesar", desc: "Pan Pretzel acompañado con pollo apanado, mozzarella y salsa de ajo, con mix de lechugas y parmesano. Servida con papas.", price: "$36.000", img: "IMAGENES/crisp.jpg" },
+    { name: "Cheese Bacon Burger", desc: "Pan Pretzel acompañado con carne burger, queso amarillo, tocineta, cebolla roja y pepinillos. Servida con papas.", price: "$33.000", img: "IMAGENES/bacon.jpg" },
+    { name: "Burger OG", desc: "Pan Pretzel acompañado con carne de 130gr, doble queso, chorizo artesanal, tocineta y salsas de la casa. Servida con papas.", price: "$48.000", img: "IMAGENES/burgerog.jpg" },
+    { name: "Spicy & Sweet Burger", desc: "Pan Pretzel acompañado con carne burger carne de 130gr, mozzarella, pimentones caramelizados y salsa de la casa. Servida con papas.", price: "$33.000", img: "IMAGENES/spicy.jpg" },
+    { name: "Burger Whopper", desc: "Pan Pretzel acompañado con carne burger carne de 130gr, mozzarella apanado, cebolla caramelizada y tocineta. Servida con papas.", price: "$46.000", img: "IMAGENES/whop.jpg" },
+    { name: "The Big Boss", desc: "Pan Pretzel acompañado con carne doble de 130gr, doble queso cheddar, doble tocineta y salsa de la casa. Servida con papas.", price: "$46.000", img: "IMAGENES/hamburguesan.png" },
+    { name: "La Patrona", desc: "Pan Pretzel acompañado con carne doble de 130gr, piña caramelizada, salsa de la casa, Dip dulce & salado, lechuga y tomate. Servida con papas.", price: "$48.000", img: "IMAGENES/lapatro.jpg" },
   ],
 
   HOTDOG: [
